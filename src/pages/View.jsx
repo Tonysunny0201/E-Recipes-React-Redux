99f4c4979990efed8df4daf10d65c7c505cd3aac
Tony Sunny <tonysunny0201@gmail.com>
2024-10-28 13:23:33 +0530

    Update View.jsx

--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import Header from '../components/Header'
+import Header from '../Components/Header'
 import { useParams } from 'react-router-dom'
 
 
