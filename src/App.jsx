3132ab4ca6c89b6b1ef3a7fe7c8ed8ed95e46e97
Tony Sunny <tonysunny0201@gmail.com>
2024-10-28 13:27:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import View from './pages/View'
-import Footer from './components/Footer'
+import Footer from './Components/Footer'
 
 function App() {
 
